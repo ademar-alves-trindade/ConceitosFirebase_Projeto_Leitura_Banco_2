c0fe06b87c6a680b30060b3a8a4969060da9bd7f
trindade <ademar.alves@unemat.br>
2023-06-18 19:33:08 -0300

    ConceitosFirebase_Projeto_Leitura_BancoDados_2

---
+++ app/src/main/java/br/com/ademar/conceitosfirebase/User.java
@@ -0,0 +1,17 @@
+package br.com.ademar.conceitosfirebase;
+
+public class User {
+    String firstName, lastName, age;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getAge() {
+        return age;
+    }
+}
